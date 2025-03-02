f45678219dea47edfa7aecc604d1663678cdc5c6
PUROHIT LOKESH SHIVKUMAR <144609651+LokeshDotDev@users.noreply.github.com>
2025-03-02 16:01:11 +0530

    virtual-class project - ✅

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/blackhole_camera_logo.svg"
+          width={50}
+          height={50}
+          alt="virtual-class logo"
+          className="max-sm:size-12"
+        />
+        <p className="text-[26px] font-extrabold text-white">VIRTUAL-CLASS</p>
+      </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
